66b9816ed1db5f2b11bb8319bfb14477858be11c
Soeren Herrmann <33161332+PixelWelt@users.noreply.github.com>
2021-09-12 14:26:52 +0200

    created substantiv object

--- src/me/soeren/Substantiv.java
+++ src/me/soeren/Substantiv.java
@@ -1,4 +1,15 @@
 package me.soeren;
 
 public class Substantiv {
+    Genus genus;
+    String genitiv;
+    String bedeutung;
+    String grundform;
+
+        public Substantiv(String grundform, Genus genus, String genitiv, String bedeutung){
+            this.grundform = grundform;
+            this.genus = genus;
+            this.genitiv = genitiv;
+            this.bedeutung = bedeutung;
+        }
 }
